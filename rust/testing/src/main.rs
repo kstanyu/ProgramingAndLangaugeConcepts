efd99e7ee0ec183de9327953a5ff689ffd38d87b
Kingsly Tanyu <kstanyu@gmail.com>
2020-03-29 16:40:32 -0500

    fibonnaci function accurately generates any term of the fib sequence -[KT]

--- rust/testing/src/main.rs
+++ rust/testing/src/main.rs
@@ -3,7 +3,15 @@ pub fn add (a: i32, b: i32) -> i32 {
 }
 
 fn subtraction (a: i32, b: i32) -> i32 {
-   a - b
+    a - b
+}
+
+fn fibonacci (a: u32) -> u32 {
+    if a != 0 && a != 1 {
+        print!("im the value of a { }", a);
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
+    a
 }
 
 #[cfg(test)]
@@ -18,6 +26,25 @@ mod tests {
     #[test]
     fn test_subtraction() {
         assert_eq!(subtraction(1,2), -1)
+    }
+
+    #[test]
+    fn test_fibonacci_sequence_first_term() {
+        assert_eq!(fibonacci(0), 0);
+    }
+    #[test]
+    fn test_fibonacci_sequence_second_term() {
+        assert_eq!(fibonacci(1), 1);
+    }
+
+    #[test]
+    fn test_fibonacci_sequence_third_term() {
+        assert_eq!(fibonacci(2), 1);
+    }
+
+    #[test]
+    fn test_fibonacci_sequence_nth_term() {
+        assert_eq!(fibonacci(3), 2);
     }
 }
 
